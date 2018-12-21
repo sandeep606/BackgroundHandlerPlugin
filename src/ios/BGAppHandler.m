d37076546d05f637118b24713cc486d39cb42917
sandeep <sandeep.rawat@evontech.com>
2018-12-21 15:40:53 +0530

    initial commit

---
+++ src/ios/BGAppHandler.m
@@ -0,0 +1,37 @@
+/********* BGAppHandler.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface BGAppHandler : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)startAppInBackground:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation BGAppHandler
+
+- (void)startAppInBackground:(CDVInvokedUrlCommand*)command
+{
+
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Success occured"];
+    
+    UIBackgroundTaskIdentifier bgTask = UIBackgroundTaskInvalid;
+    bgTask = [[UIApplication sharedApplication]
+         beginBackgroundTaskWithExpirationHandler:^{
+             [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+    }];
+   
+   [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+    // send to server with a synchronous request
+
+    // AFTER ALL THE UPDATES, close the task
+    // if (bgTask != UIBackgroundTaskInvalid)
+    // {
+    //     [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+    // }
+}
+
+
+@end
